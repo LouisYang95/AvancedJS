0c4c6e8087807e8c1671948fa11990314c126f3c
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-01-12 13:07:42 +0100

    Update app.js
    Project Memory card almost finish version !

---
+++ MemoryCard/app.js
@@ -0,0 +1,77 @@
+const carte = document.querySelectorAll(".carte"); //we get our class "carte" from the html element
+let carteRetourne = false;  //defines a boolean to know if our card is flipped
+let memoire1, memoire2;     //defines var for firstCard and secondCard
+let verouille = false;
+let doublon = 0;
+
+
+randomizeBoard();
+
+carte.forEach(card => card.addEventListener('click', returnCard));  //each time we click on the card
+
+//---------------------------- Function to flip our card ----------------------//
+function returnCard(){
+    let card2 = event.target.parentNode; //get "arriere" parentNode
+    if (verouille) return;  //return verouille values
+    if (this === memoire1) return;  //return first card values if our selected card equal to memoire1
+    card2.classList.toggle('active');   //give active class , it flip the card
+    //condition for pick card
+    if(!carteRetourne){
+        carteRetourne = true;
+        memoire1 = this;
+        console.log(memoire1);
+        return;
+    }
+        memoire2 = this;
+        carteRetourne = false;
+        console.log(memoire2)
+//match part : we compare our attribute , if same then we can't click on the card to RE-flip
+    if (memoire1.getAttribute("data-attr") === memoire2.getAttribute("data-attr")){
+        memoire1.removeEventListener("click",returnCard);
+        memoire2.removeEventListener("click",returnCard);
+        console.log(memoire1 + " " +memoire2)
+        doublon +=1; // add a pair
+        suppBoard(); // reset the board
+// if not the same , cancel the flip effect
+    }else{
+        verouille = true;
+        setTimeout(() => {
+            memoire1.childNodes[1].classList.remove('active');
+            memoire2.childNodes[1].classList.remove('active');;
+            suppBoard();
+        }, 500);
+    }
+    victoire(); 
+}
+//--------------------------function to alert us when we win------------------------------//
+
+function victoire(){
+    if (doublon >= 6){ //if pair is equal to 6 (we have 12 cards)
+        setTimeout(() => {alert("Vous avez gagné la partie ! \n Bien joué !")
+        var btn = document.createElement("Button");
+        btn.innerHTML = " Recommencer";
+        btn.style.color="strong lightblue";
+        btn.style.backgroundColor="white";
+        btn.style.width="150px";
+        btn.style.height="80px";
+        btn.style.textAlign="center";
+        document.body.appendChild(btn); //create a button restart to restart game
+        btn.addEventListener('click', function (e) { 
+            //it reload page and will randomize by reloading thanks to function randomizeBoard
+            window.location.reload();
+        })
+     },400)}
+}
+//-----------------------------Function to reset board's values -----------------------------------//
+function suppBoard() {
+    carteRetourne = false; //set carteRetourne to false
+    verouille = false;      //set verouille to false
+    memoire1 = null;    //set memoire1 to null 
+    memoire2 = null;    //set memoire2 to null
+  }
+//-----------------------------Function who randomizes board ------------------------------------//
+function randomizeBoard() {
+    carte.forEach(card => {
+        card.style.order = Math.floor(Math.random() * 12); //randomize each card order
+    });
+};
