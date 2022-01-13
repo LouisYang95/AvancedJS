0a699f11702349f9ea0b54d40d9ad37422fc2615
Antony <91181127+Getsugo@users.noreply.github.com>
2022-01-13 16:24:36 +0100

    V6
    bon mouvement des aliens

--- Space Invaders/app.js
+++ Space Invaders/app.js
@@ -7,6 +7,11 @@ let height = 40
 let widthAlien = 1
 let invadersId
 const resultasDisplay = document.querySelector('.resultas')
+var avancer = 0;
+var direction = 1;
+let WallRight = [19, 39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 239, 259]
+
+
 
 
 
@@ -74,17 +79,42 @@ function moveShooterLeftReightUpDown() {
 document.addEventListener('keydown', moveShooterLeftReightUpDown)
 
 
-/*Mouvement des aliens */
 function moveInvaders() {
 
-    remove()
+    remove();
 
-    for (let i = 0; i < alienInvaders.length; i++) {
-        alienInvaders[i] += widthAlien
-
+    if (avancer == 0) { // aller a gauche
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += widthAlien;
+            console.log(aliensRemoved.includes(i));
+            for (let y = 0; y < WallRight.length; y++) {
+                if (alienInvaders[i] == WallRight[y] && !aliensRemoved.includes(i)) {
+                    avancer = 2;
+                    direction = 1;
+                }
+            }
+        }
+    } else if (avancer == 1) { // aller a droite
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] -= widthAlien;
+            if (alienInvaders[i] % 20 == 0 && !aliensRemoved.includes(i)) {
+                avancer = 2;
+                direction = 2;
+            }
+        }
+    } else if (avancer == 2) { // s'arreter
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += widthAlien + 19;
+        }
+        if (direction == 1) {
+            avancer = 1;
+        } else if (direction == 2) {
+            avancer = 0;
+        }
     }
+    draw();
 
-    draw()
+
 
     if (squares[currentShooterIndex].classList.contains('alien', 'tireur')) {
         resultasDisplay.innerHTML = 'GAME OVER'
@@ -98,7 +128,7 @@ function moveInvaders() {
 }
 
 /*Vistesse de mouvement des aliens */
-invadersId = setInterval(moveInvaders, 700)
+invadersId = setInterval(moveInvaders, 100)
 
 
 
